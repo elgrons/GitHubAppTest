aa59394c9833cade3cd4730be4095667636acb1e
Eliot Gronstal <eliot.lauren@gmail.com>
2023-06-16 13:42:47 -0700

    lets goooooooooooooooooooooooo

--- src/index.js
+++ src/index.js
@@ -34,11 +34,6 @@ window.addEventListener("load", function() {
   document.querySelector("#rectangle-area-form").addEventListener("submit", handleRectangleForm);
 });
 
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//T'S GET THIS TESTasdfasdfasdfasdf//LET'S GET THIS TESTasdfasdfasdfa
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//LET'S GET THIS TESTasdfasdfasdfasdf
 //T'S GET THIS TESTasdfasdfasdfasdf//LET'S GET THIS TESTasdfasdfasdfa
 //LET'S GET THIS TESTasdfasdfasdfasdf
 //T'S GET THIS TESTasdfasdfasdfasdf//LET'S GET THIS TESTasdfasdfasdfa
