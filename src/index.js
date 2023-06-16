ab5a285648359c567e9b3c4f2ca4f812671f2277
Eliot Gronstal <eliot.lauren@gmail.com>
2023-06-16 13:58:31 -0700

    update i dare you

--- src/index.js
+++ src/index.js
@@ -42,10 +42,3 @@ window.addEventListener("load", function() {
 //LET'S GET THIS TESTasdfasdfasdfasdf
 //LET'S GET THIS TESTasdfasdfasdfasdf
 //LET'S GET THIS TESTasdfasdfasdfasdf
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//T'S GET THIS TESTasdfasdfasdfasdf//LET'S GET THIS TESTasdfasdfasdfa
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//T'S GET THIS TESTasdfasdfasdfasdf//LET'S GET THIS TESTasdfasdfasdfa
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//LET'S GET THIS TESTasdfasdfasdfasdf
