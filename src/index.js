e5cecb5facd9279da8dd5e6da02a425abaed260c
Eliot Gronstal <eliot.lauren@gmail.com>
2023-06-19 18:40:25 -0700

    monday funday.

--- src/index.js
+++ src/index.js
@@ -52,15 +52,7 @@ window.addEventListener("load", function() {
 //LET'S GET THIS TESTasdfasdfasdfasdf
 //LET'S GET THIS TESTasdfasdfasdfasdf
 //T'S GET THIS TESTasdfasdfasdfasdf//LET
-//T'S GET THIS TESTasdfasdfasdfasdf//LET'S GET THIS TESTasdfasdfasdfa
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//T'S GET THIS TESTasdfasdfasdfasdf//LET
-//T'S GET THIS TESTasdfasdfasdfasdf//LET'S GET THIS TESTasdfasdfasdfa
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//LET'S GET THIS TESTasdfasdfasdfasdf
-//T'S GET THIS TESTasdfasdfasdfasdf//LET
+
 //T'S GET THIS TESTasdfasdfasdfasdf//LET'S GET THIS TESTasdfasdfasdfa
 //LET'S GET THIS TESTasdfasdfasdfasdf
 //LET'S GET THIS TESTasdfasdfasdfasdf
